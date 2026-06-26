63a1ea8ec59314cbe910b2554c74927666782499
Sabari S <sabarichill007@gmail.com>
2026-06-26 15:55:09 +0530

    hardcode production API URL

--- src/services/api.js
+++ src/services/api.js
@@ -3,16 +3,12 @@ import axios from "axios";
 const BASE_URL =
   "https://student-database-backend-q7gm.onrender.com/api/students";
 
-// All backend API calls centralized here
 const api = {
-  // CRUD
   getAll: () => axios.get(BASE_URL),
   getById: (id) => axios.get(`${BASE_URL}/${id}`),
   create: (student) => axios.post(BASE_URL, student),
   update: (id, student) => axios.put(`${BASE_URL}/${id}`, student),
   delete: (id) => axios.delete(`${BASE_URL}/${id}`),
-
-  // Extra features
   search: (keyword) => axios.get(`${BASE_URL}/search?keyword=${keyword}`),
   sort: (sortBy, order) =>
     axios.get(`${BASE_URL}/sort?sortBy=${sortBy}&order=${order}`),
